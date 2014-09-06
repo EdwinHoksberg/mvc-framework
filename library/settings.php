889e4ce33470beb9d58b659a984d98be4a43c8b4
Edwin <edwin@vacatureboard.com>
2014-09-06 17:42:52 +0200

    Added function to add settings

--- library/settings.php
+++ library/settings.php
@@ -12,10 +12,19 @@
  */
 final class Settings extends Router {
 
-    public static function get($name = null) {
+    /**
+     * This function will get any settings from the database.
+     * You should set the forceUpdate parameter to true if you inserted
+     * an new value into te database, or it won't be available.
+     *
+     * @param null $name
+     * @param bool $forceUpdate
+     * @return string|bool
+     */
+    public static function get($name = null, $forceUpdate = false) {
         static $data = array();
 
-        if (empty($data)) {
+        if (empty($data) || $forceUpdate) {
             $db = new Database();
 
             $result = $db->select("
@@ -34,5 +43,47 @@ final class Settings extends Router {
         } else {
             return false;
         }
+    }
+
+    /**
+     * This function will insert an new value into the database,
+     * and if it already exists, it will overwrite it.
+     *
+     * @param $name - The value name
+     * @param $value - The value
+     * @return bool
+     */
+    public static function set($name, $value) {
+
+        $db = new Database();
+
+        $alreadyExists = $db->select("SELECT `name` FROM {DB_PREFIX}settings WHERE `name` = :name",
+            array(
+                ":name" => $name
+            )
+        );
+
+        if (count($alreadyExists) > 0) { // update existing setting
+
+            return $db->update("settings",
+                array( // values
+                    "name" => $name,
+                    "value" => $value
+                ),
+                array( // where
+                    "name" => $name
+                )
+            );
+        } else { // insert new setting in database
+
+            return $db->insert("settings",
+                array( // values
+                    "name" => $name,
+                    "value" => $value
+                )
+            );
+        }
+
+
     }
 }
